586aaa74b1eecc2f0df5a9371f98ecd80ff6edcd
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-01-30 12:09:00 -0500

    Fix class name

---
+++ AcutePediatricsOrientation/ViewModels/EditPackageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using AcutePediatricsOrientation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcutePediatricsOrientation.ViewModels
+{
+    public class EditPackageViewModel
+    {
+        public List<EditPackageViewModelCategory> Categories { get; set; }
+    }
+
+    public class EditPackageViewModelCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<EditPackageViewModelTopic> Topics { get; set; }
+    }
+
+    public class EditPackageViewModelTopic
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<EditPackageViewModelDocuments> Documents { get; set; }
+    }
+
+    public class EditPackageViewModelDocuments
+    {
+        public string Name { get; set; }
+    }
+}
