63e3359a0de21bba70f2e73286268e25f12b51ff
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-01-30 10:04:28 -0500

    Add the login and logout functionality

--- AcutePediatricsOrientation/Controllers/AccountController.cs
+++ AcutePediatricsOrientation/Controllers/AccountController.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using AcutePediatricsOrientation.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 
 namespace AcutePediatricsOrientation.Controllers
 {
@@ -18,9 +21,51 @@ namespace AcutePediatricsOrientation.Controllers
             _context = context;
         }
 
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _context.Account.SingleOrDefault(a => a.Username == account.Username && a.Password == account.Password);
+
+                if(user == null)
+                {
+                    ModelState.AddModelError("", "username or password is invalid");
+                    return View();
+                }
+                else
+                {
+                    // Create the identity from the user info
+                    var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+                    identity.AddClaim(new Claim(ClaimTypes.Name, account.Username));
+                    if(user.Role == "ADMIN")
+                    {
+                        identity.AddClaim(new Claim(ClaimTypes.Role, "Admin"));
+                    }
+
+                    // Authenticate using the identity
+                    var principal = new ClaimsPrincipal(identity);
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties { IsPersistent = false });
+
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
         }
     }
 }
