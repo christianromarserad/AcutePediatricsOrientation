2d77df93910151b25ef4d7ae53c2c312ed365f7b
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-01-30 10:50:31 -0500

    Add EF models: Category, Documents, Topics

---
+++ AcutePediatricsOrientation/Models/Documents.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcutePediatricsOrientation.Models
+{
+    public class Documents
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string FilePath { get; set; }
+        public string TopicId { get; set; }
+        public virtual Topic Topic { get; set; }
+
+    }
+}
---
+++ AcutePediatricsOrientation/Models/Topic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcutePediatricsOrientation.Models
+{
+    public class Topic
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CategoryId { get; set; }
+        public virtual ICollection<Documents> Documents { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
