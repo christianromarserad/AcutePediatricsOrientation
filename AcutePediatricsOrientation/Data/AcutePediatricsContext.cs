61656240722e62e3bb64e0264f73307a9698e67e
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-01-30 08:47:49 -0500

    Adding Entity Framework

---
+++ AcutePediatricsOrientation/Data/AcutePediatricsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcutePediatricsOrientation.Models
+{
+    public class AcutePediatricsContext : DbContext
+    {
+        public AcutePediatricsContext (DbContextOptions<AcutePediatricsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AcutePediatricsOrientation.Models.Account> Account { get; set; }
+    }
+}
