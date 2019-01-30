5c12d61f67e9319534bd94b0a0c33344113437db
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-01-30 10:41:22 -0500

    Add tables in the database: Category, Topic, Document

---
+++ Database1/Document.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Document]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(50) NOT NULL, 
+    [Type] NCHAR(50) NOT NULL, 
+    [FilePath] NCHAR(50) NOT NULL, 
+    [TopicId] INT NOT NULL, 
+    CONSTRAINT [FK_Document_Topic] FOREIGN KEY ([TopicId]) REFERENCES [Topic]([Id])
+)
---
+++ Database1/Topic.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Topic]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(50) NOT NULL, 
+    [CategoryId] INT NOT NULL, 
+    CONSTRAINT [FK_Topic_Category] FOREIGN KEY ([CategoryId]) REFERENCES [Category]([Id])
+)
