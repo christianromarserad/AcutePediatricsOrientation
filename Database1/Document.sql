66bdec482c17a9015523bebb92df98efd6baaa22
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-02-27 09:59:45 -0500

    Fix bug where inserting a record in Document table causes error

--- Database1/Document.sql
+++ Database1/Document.sql
@@ -3,7 +3,7 @@
 	[Id] INT NOT NULL IDENTITY PRIMARY KEY, 
     [Name] NCHAR(50) NOT NULL, 
     [DocumentTypeId] INT NOT NULL, 
-    [FilePath] NCHAR(50) NOT NULL, 
+    [FilePath] NCHAR(300) NOT NULL, 
     [TopicId] INT NOT NULL, 
     CONSTRAINT [FK_Document_Topic] FOREIGN KEY ([TopicId]) REFERENCES [Topic]([Id]), 
     CONSTRAINT [FK_Document_DocumentType] FOREIGN KEY ([DocumentTypeId]) REFERENCES [DocumentType]([Id])
