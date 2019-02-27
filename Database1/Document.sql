04b42682ef3d28468ab6d97a13f7398126ff0577
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-02-27 07:50:49 -0500

    Add new database table: DocumentType

--- Database1/Document.sql
+++ Database1/Document.sql
@@ -2,8 +2,9 @@
 (
 	[Id] INT NOT NULL IDENTITY PRIMARY KEY, 
     [Name] NCHAR(50) NOT NULL, 
-    [Type] NCHAR(50) NOT NULL, 
+    [DocumentTypeId] INT NOT NULL, 
     [FilePath] NCHAR(50) NOT NULL, 
     [TopicId] INT NOT NULL, 
-    CONSTRAINT [FK_Document_Topic] FOREIGN KEY ([TopicId]) REFERENCES [Topic]([Id])
+    CONSTRAINT [FK_Document_Topic] FOREIGN KEY ([TopicId]) REFERENCES [Topic]([Id]), 
+    CONSTRAINT [FK_Document_DocumentType] FOREIGN KEY ([DocumentTypeId]) REFERENCES [DocumentType]([Id])
 )
---
+++ Database1/DocumentType.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[DocumentType]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(10) NOT NULL
+)
