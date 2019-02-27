3d81371596c3c8fb91e89e50004a6c3cff155fef
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-02-27 07:45:41 -0500

    Add new database table: Role

--- Database1/Account.sql
+++ Database1/Account.sql
@@ -3,5 +3,6 @@
 	[Id] INT NOT NULL IDENTITY PRIMARY KEY, 
     [Username] NCHAR(50) NOT NULL, 
     [Password] NCHAR(50) NOT NULL, 
-    [Role] NCHAR(50) NOT NULL
+    [RoleId] INT NOT NULL, 
+    CONSTRAINT [FK_Account_Role] FOREIGN KEY ([RoleId]) REFERENCES [Role]([Id])
 )
