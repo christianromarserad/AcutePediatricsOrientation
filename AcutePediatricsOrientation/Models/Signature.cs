544532a063422bcacfa6c16712cebfbc0a818405
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-03-07 08:39:42 -0500

    Add signature  model

---
+++ AcutePediatricsOrientation/Models/Signature.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcutePediatricsOrientation.Models
+{
+    public class Signature
+    {
+        public int UserId { get; set; }
+        public int TopicId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
