17beb5b847bbed79bb42188df373063b4b0bf4ac
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-03-08 17:52:57 -0500

    Change signature date to datetime

--- Database1/Signature.sql
+++ Database1/Signature.sql
@@ -2,7 +2,7 @@
 (
 	[UserId] INT NOT NULL, 
     [TopicId] INT NOT NULL, 
-    [Date] DATE NOT NULL
+    [Date] DATETIME NOT NULL
 	CONSTRAINT PK_Person PRIMARY KEY ([UserId],[TopicId]), 
     CONSTRAINT [FK_Signature_User] FOREIGN KEY ([UserId]) REFERENCES [Account]([Id]), 
     CONSTRAINT [FK_Signature_Topic] FOREIGN KEY ([TopicId]) REFERENCES [Topic]([Id])
