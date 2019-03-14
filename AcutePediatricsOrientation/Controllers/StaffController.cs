4b8392211c48256ce42c99108e0d055e7b7c44b0
Christian <43688000+christianromarserad@users.noreply.github.com>
2019-03-14 11:58:52 -0400

    Improve the staff view

--- AcutePediatricsOrientation/Controllers/StaffController.cs
+++ AcutePediatricsOrientation/Controllers/StaffController.cs
@@ -1,16 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using AcutePediatricsOrientation.Models;
+using AcutePediatricsOrientation.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AcutePediatricsOrientation.Controllers
 {
     public class StaffController : Controller
     {
+        private readonly AcutePediatricsContext _context;
+
+        public StaffController(AcutePediatricsContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var staffListViewModel = new StaffListViewModel();
+            var stafSignaturefList = _context.Signature.ToList();
+            var totalTopics = (double)_context.Topic.Count();
+            staffListViewModel.Users = _context.Account.Select(a => 
+                new StaffViewModel
+                {
+                    UserId = a.Id,
+                    UserName = a.Username,
+                    Progress = (((double)stafSignaturefList.Where(sl => sl.UserId == a.Id).Count() / totalTopics) * 100.0)
+                }
+            ).ToList();
+
+            return View(staffListViewModel);
         }
     }
 }
---
+++ AcutePediatricsOrientation/ViewModels/AccountViewModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcutePediatricsOrientation.ViewModels
+{
+    public class StaffListViewModel
+    {
+        public List<StaffViewModel> Users { get; set; }
+    }
+
+    public class StaffViewModel
+    {
+        public string UserName { get; set; }
+        public int UserId { get; set; }
+        public double Progress { get; set; }
+    }
+}
